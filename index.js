9585acabc4ef1f419e579f46ae8f40d17df7a907
marothon <marothon@gmail.com>
2024-07-29 14:15:25 +0200

    Add validation of mandatory inputs

--- index.js
+++ index.js
@@ -12,3 +12,31 @@ document.querySelector('h3#register').addEventListener('click', (e) => {
     document.querySelector('form#register').classList.add('active');
     e.target.classList.add('active');
 });
+
+document.querySelector('form#register').addEventListener('submit', (e) => {
+    e.preventDefault();
+    // Validate
+    allInputsHaveValues(e.target)
+});
+
+document.querySelector('form#login').addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+function allInputsHaveValues(formElement){
+    let valid = true; 
+    let data = new FormData(formElement);
+    for (const [name, value] of data) {
+        if(value === ''){
+            valid = false;
+            highlightInvalidInput(formElement, name);
+        }
+    }
+}
+
+function highlightInvalidInput(formElement, inputName){
+    formElement.querySelector(`input[name="${inputName}"]`).classList.add('invalid');
+    formElement.querySelector(`input[name="${inputName}"]`).addEventListener('focus', (e) => {
+        e.target.classList.remove('invalid');
+    }, {once: true});
+}
